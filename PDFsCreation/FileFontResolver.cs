9596248d2cbb8a9a1ce75f980c6f45146710b3c9
Sina Hadi <sinahadi01@gmail.com>
2024-11-14 02:13:01 +0330

    Add project files.

---
+++ PDFsCreation/FileFontResolver.cs
@@ -0,0 +1,89 @@
+﻿using PdfSharp.Fonts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFsCreation
+{
+    public class FileFontResolver : IFontResolver // FontResolverBase
+    {
+        public string DefaultFontName => throw new NotImplementedException();
+
+        public byte[] GetFont(string faceName)
+        {
+            using (var ms = new MemoryStream())
+            {
+                using (var fs = File.Open(faceName, FileMode.Open))
+                {
+                    fs.CopyTo(ms);
+                    ms.Position = 0;
+                    return ms.ToArray();
+                }
+            }
+        }
+
+        public FontResolverInfo ResolveTypeface(string familyName, bool isBold, bool isItalic)
+        {
+            if (familyName.Equals("Verdana", StringComparison.CurrentCultureIgnoreCase))
+            {
+                if (isBold && isItalic)
+                {
+                    return new FontResolverInfo("Fonts/Verdana.ttf");
+                }
+                else if (isBold)
+                {
+                    return new FontResolverInfo("Fonts/Verdana.ttf");
+                }
+                else if (isItalic)
+                {
+                    return new FontResolverInfo("Fonts/Verdana.ttf");
+                }
+                else
+                {
+                    return new FontResolverInfo("Fonts/Verdana.ttf");
+                }
+            }
+            if (familyName.Equals("Arial", StringComparison.CurrentCultureIgnoreCase))
+            {
+                if (isBold && isItalic)
+                {
+                    return new FontResolverInfo("Fonts/Arial.ttf");
+                }
+                else if (isBold)
+                {
+                    return new FontResolverInfo("Fonts/Arial.ttf");
+                }
+                else if (isItalic)
+                {
+                    return new FontResolverInfo("Fonts/Arial.ttf");
+                }
+                else
+                {
+                    return new FontResolverInfo("Fonts/Arial.ttf");
+                }
+            }
+            if (familyName.Equals("Courier New", StringComparison.CurrentCultureIgnoreCase))
+            {
+                if (isBold && isItalic)
+                {
+                    return new FontResolverInfo("Fonts/Cour.ttf");
+                }
+                else if (isBold)
+                {
+                    return new FontResolverInfo("Fonts/Cour.ttf");
+                }
+                else if (isItalic)
+                {
+                    return new FontResolverInfo("Fonts/Cour.ttf");
+                }
+                else
+                {
+                    return new FontResolverInfo("Fonts/Cour.ttf");
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ PDFsCreation/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
+using PDFsCreation;
+using PdfSharp.Fonts;
+using System.Diagnostics;
+
+Document document = CreatePdf.CreateDocument();
+
+MigraDoc.DocumentObjectModel.IO.DdlWriter.WriteToFile(document, "MigraDoc.mdddl");
+
+PdfDocumentRenderer renderer = new PdfDocumentRenderer();
+renderer.Document = document;
+
+GlobalFontSettings.FontResolver = new FileFontResolver();
+
+renderer.RenderDocument();
+
+// Save the document...
+string filename = "C:\\Users\\sinah\\source\\repos\\PDFsCreation\\PDFsCreation\\Images\\HelloMigraDoc.pdf";
+renderer.PdfDocument.Save(filename);
+// ...and start a viewer.
+//Process.Start(filename);
